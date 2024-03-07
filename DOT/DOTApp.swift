50dbfd8612aac144dda1d16d257a24353f5a85ef
mnava1418 <martin@mnavapena.com>
2024-03-06 19:09:00 -0500

    Initial Commit

---
+++ DOT/DOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOTApp.swift
+//  DOT
+//
+//  Created by Martin Nava on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
