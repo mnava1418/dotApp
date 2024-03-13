f58d05c9289e4762cd14680d6d36e5f04eaf9702
mnava1418 <martin@mnavapena.com>
2024-03-12 23:20:59 -0400

    UPDATE: object to control modal status

--- DOT/models/Modal.swift
+++ DOT/models/Modal.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class Modal: ObservableObject {
+    @Published public var show: Bool = false
+    @Published public var title: String = ""
+    @Published public var text: String = ""
+    @Published public var btnLabel: String = ""
+    
+    public func setModal(_title: String, _text: String, _btnLabel: String) {
+        self.show = true
+        self.title = _title
+        self.text = _text
+        self.btnLabel = _btnLabel
+    }
+}
