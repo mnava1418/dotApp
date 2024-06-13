d3aba5e8c084fe2fdf86f3c930c5b7c6cfe065c2
mnava1418 <martin@mnavapena.com>
2024-06-13 18:51:10 -0400

    ADD: create home view

---
+++ DOT/views/HomeView.swift
@@ -0,0 +1,62 @@
+//
+//  HomeView.swift
+//  DOT
+//
+//  Created by Martin Nava on 11/06/24.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct HomeView: View {
+    @State private var user:User? = nil
+    @State private var userName = "Martin"
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.AppColors.darkMain, Color.AppColors.main]),
+                                   startPoint: .topLeading,
+                                   endPoint: .bottomTrailing)
+                        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                HStack {
+                    Text("Hola \(userName),")
+                        .font(.system(size: 40))
+                        .bold()
+                        .foregroundColor(Color.AppColors.text)
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.top, 20)
+                
+                Spacer()
+                
+                VStack {
+                    Button(action: {
+                        // Acción del botón
+                        print("Botón presionado")
+                    }) {
+                        Image("dot")
+                         .resizable()
+                         .frame(width: 200, height: 200)
+                         .padding()
+                    }
+                 }
+                
+                Spacer()
+            }
+            .onAppear{
+                user = Auth.auth().currentUser
+                
+                if let currUser = user, let displayName = currUser.displayName {
+                    userName = displayName
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
