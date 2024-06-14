4039048d8bc306cb3cdd383917cb3b0140398a26
mnava1418 <martin@mnavapena.com>
2024-06-13 22:27:18 -0400

    ADD: set conectivity bewtween phone and watch

---
+++ DOT/services/WatchConectivityManager.swift
@@ -0,0 +1,37 @@
+//
+//  WatchConectivityManager.swift
+//  DOT
+//
+//  Created by Martin Nava on 13/06/24.
+//
+
+import Foundation
+import WatchConnectivity
+
+class WatchConnectivityManager: NSObject, WCSessionDelegate {
+    static let shared = WatchConnectivityManager()
+    
+    private override init() {
+        super.init()
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+    }
+    
+    func sendMessage(message: [String: Any]) {
+        if WCSession.default.isReachable {
+            WCSession.default.sendMessage(message, replyHandler: nil) { error in
+                print("Error sending message: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // WCSessionDelegate methods
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
+    func sessionDidBecomeInactive(_ session: WCSession) {}
+    func sessionDidDeactivate(_ session: WCSession) {
+        session.activate()
+    }
+}
